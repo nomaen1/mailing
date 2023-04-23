806de96d94e92b1a19dcedbb60ff5ac642164a41
Janysh <tynybekovjanyshbek@gmail.com>
2023-04-23 19:39:52 +0600

    parsingglobus

--- parsing_bot.py
+++ parsing_bot.py
@@ -1,10 +1,8 @@
 from aiogram import Bot, Dispatcher, types, executor
 from dotenv import load_dotenv
-import os
-import logging
 from bs4 import BeautifulSoup
-import requests
-import parsing_aki
+import requests, os, logging
+
 
 load_dotenv('.env')
 
@@ -25,8 +23,23 @@ async def get_news(message:types.Message):
     n = 0
     for news in quotes:
         n += 1
+        link = news.get('href')
         with open('parsing.txt', 'a+', encoding="utf-8") as file:
             file.write(f"{n}) {news.text}\n")
-            await message.answer(f"{n}) {news.text}\n")
+            await message.answer(f"{n}) {news.text}\n{link}")
+
+@dp.message_handler(commands = 'products')
+async def get_products(message:types.Message):
+    await message.reply("hhh")
+    url = "https://globus-online.kg/catalog/ovoshchi_frukty_orekhi_zelen/ovoshchi/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    quotes = soup.find_all('div', class_= 'list-showcase__part-main')
+    n = 0
+    for prod in quotes:
+        n += 1
+        title = prod.find('div', class_='list-showcase__name')
+        price = prod.find('span', class_='c-prices__value js-prices_pdv_ГЛОБУС Розничная')
+        await message.answer(f"{n}) {title.text}\n{price.text}")
 
 executor.start_polling(dp)
